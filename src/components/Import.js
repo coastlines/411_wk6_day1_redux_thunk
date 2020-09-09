2a66334347ee0178e88c3b08b3a2f022706b09c7
efaulkenberry@gmail.com <efaulkenberry@gmail.com>
2020-09-09 00:21:50 -0500

    hide ttable header until import button is clicked

--- src/components/Import.js
+++ src/components/Import.js
@@ -12,28 +12,55 @@ import {
 import ImportActionMenu from "./ImportActionMenu";
 
 const Import = (props) => {
+  // Hide table header until the import button is clicked (lines 17, 19, 24, 41, 46)
+  const [showTable, setShowTable] = React.useState(false);
+
+  const displayTable = () => {
+    props.fetchMakes();
+    setShowTable(true);
+  };
+
+  const DisplayTableHead = () => {
+    return (
+      <TableHead id="import-table-head">
+        <TableRow>
+          <TableCell align="right">ID</TableCell>
+          <TableCell align="left">Make</TableCell>
+          <TableCell align="right">Actions</TableCell>
+        </TableRow>
+      </TableHead>
+    );
+  };
+
+  const DefaultDisplay = () => {
+    return (
+      <h3 className="import-default-display">
+        Click the import button above to show all current makes.
+      </h3>
+    );
+  };
+
   return (
     <div className="import-container">
       <Container component="Paper">
         <div className="import-header">
           <h2>Total Makes: {props.makes.length}</h2>
-          <Button variant="contained" color="primary" className="import-btn" onClick={props.fetchMakes}>
+          <Button
+            variant="contained"
+            color="primary"
+            className="import-btn"
+            onClick={displayTable}
+          >
             Import
           </Button>
         </div>
         <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell align="right">ID</TableCell>
-              <TableCell align="right">Make</TableCell>
-              <TableCell align="right">Actions</TableCell>
-            </TableRow>
-          </TableHead>
+          {showTable ? <DisplayTableHead /> : <DefaultDisplay />}
           <TableBody>
             {props.makes.map((make, index) => (
               <TableRow key={make.id}>
                 <TableCell align="right">{make.MakeId}</TableCell>
-                <TableCell align="right">{make.MakeName}</TableCell>
+                <TableCell align="left">{make.MakeName}</TableCell>
                 <TableCell align="right">
                   <ImportActionMenu
                     deleteMake={props.deleteMake}
