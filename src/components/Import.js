e043917007fb5026124fc097eefdf3e1d9784011
efaulkenberry@gmail.com <efaulkenberry@gmail.com>
2020-09-08 20:54:49 -0500

    import.js table

--- src/components/Import.js
+++ src/components/Import.js
@@ -1,10 +1,26 @@
 import React from 'react'
+import { Button, Table, TableHead, TableBody, TableRow, TableCell, Container, Paper } from '@material-ui/core'
 
 const Import = (props) => {
     // fill out this component
 
     return (
-        <p>Import Component</p>
+        <div>
+            <p>Import Component</p>
+            <Container component='Paper'>
+                <Button variant='contained' color='primary'>Import</Button>
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell align="right">ID</TableCell>
+                            <TableCell align="right">Make</TableCell>
+                            <TableCell align="right">Actions</TableCell>
+                        </TableRow>
+                    </TableHead>
+                </Table>
+            </Container>        
+        </div>
+       
     )
 }
 
