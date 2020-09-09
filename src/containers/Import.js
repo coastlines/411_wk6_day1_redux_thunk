b159af800cd7a3794493b040d621e65d43054222
efaulkenberry@gmail.com <efaulkenberry@gmail.com>
2020-09-08 21:00:17 -0500

    import containers

--- src/containers/Import.js
+++ src/containers/Import.js
@@ -10,7 +10,7 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
   return {
-    fetchArticles: () => dispatch(fetchMakes())
+    fetchMakes: () => dispatch(fetchMakes())
   }
 }
 
